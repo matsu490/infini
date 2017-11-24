86a9f01d7e3180490ca82d98bc280f1424ab965a
Unknown <imototoriwatomodatijanai@gmail.com>
2017-11-24 10:26:58 +0900

    refine

--- main2.py
+++ main2.py
@@ -11,9 +11,6 @@ import sys
 import time
 import paho.mqtt.publish as publish
 import threading
-import os
-import os.path
-import datetime
 import numpy as np
 from params import *
 
@@ -32,17 +29,20 @@ class Beacon(threading.Thread):
     def run(self):
         time.sleep(5 * np.random.rand())
         while True:
-            tm = time.time()
-            data = [tm, self.message]
-            payload = '{{"tm":"{0}","Beacon":"{1}"}}'.format(*data)
-            print 'Beacon: {}\n'.format(payload)
-            publish.single(
-                    topic='{}/{}'.format(self.password, self.device_id),
-                    payload=payload,
-                    hostname=self.host,
-                    auth={'username': self.username,
-                        'password': self.password})
+            self._make_data()
+            self._send_data()
             time.sleep(self.period)
+
+    def _make_data(self):
+        tm = time.time()
+        self.data = [tm, self.message]
+
+    def _send_data(self):
+        payload = '{{"tm":"{0}","Beacon":"{1}"}}'.format(*self.data)
+        print 'Beacon: {}\n'.format(payload)
+        publish.single(topic='{}/{}'.format(self.password, self.device_id),
+                payload=payload, hostname=self.host,
+                auth={'username': self.username, 'password': self.password})
 
 
 class EnvironmentalInformation(threading.Thread):
@@ -53,29 +53,32 @@ class EnvironmentalInformation(threading.Thread):
         self.host = host
         self.device_id = device_id
         self.period = period
+        self.t = 0
+        self.dt = 0.1
         self.setDaemon(True)
 
     def run(self):
         time.sleep(5 * np.random.rand())
-        t = 0
-        dt = 0.1
         while True:
-            tm = time.time()
-            eiLPrs = np.random.rand() + 900 + 100*np.sin(2*np.pi*t)
-            seaPrs = np.random.rand() + 1000 + 100*np.sin(2*np.pi*t)
-            eiTemp = np.random.rand() + 20 + 10*np.sin(2*np.pi*t)
-            eiHumi = np.random.rand() + 50 + 30*np.sin(2*np.pi*t)
-            data = [tm, eiLPrs, seaPrs, eiTemp, eiHumi]
-            payload = '{{"tm":"{0}","eiLPrs":{1},"seaPrs":{2},"eiTemp":{3},"eiHumi":{4}}}'.format(*data)
-            print 'EnvInfo: {}\n'.format(payload)
-            publish.single(
-                    topic='{}/{}'.format(self.password, self.device_id),
-                    payload=payload,
-                    hostname=self.host,
-                    auth={'username': self.username,
-                        'password': self.password})
+            self._make_data()
+            self._send_data()
+            self.t += self.dt
             time.sleep(self.period)
-            t += dt
+
+    def _make_data(self):
+        tm = time.time()
+        eiLPrs = np.random.rand() + 900 + 100*np.sin(2*np.pi*self.t)
+        seaPrs = np.random.rand() + 1000 + 100*np.sin(2*np.pi*self.t)
+        eiTemp = np.random.rand() + 20 + 10*np.sin(2*np.pi*self.t)
+        eiHumi = np.random.rand() + 50 + 30*np.sin(2*np.pi*self.t)
+        self.data = [tm, eiLPrs, seaPrs, eiTemp, eiHumi]
+
+    def _send_data(self):
+        payload = '{{"tm":"{0}","eiLPrs":{1},"seaPrs":{2},"eiTemp":{3},"eiHumi":{4}}}'.format(*self.data)
+        print 'EnvInfo: {}\n'.format(payload)
+        publish.single(topic='{}/{}'.format(self.password, self.device_id),
+                payload=payload, hostname=self.host,
+                auth={'username': self.username, 'password': self.password})
 
 
 class DigitalSensors(threading.Thread):
@@ -91,25 +94,28 @@ class DigitalSensors(threading.Thread):
     def run(self):
         time.sleep(5 * np.random.rand())
         while True:
-            tm = time.time()
-            d1 = np.random.randint(0, 2)
-            d2 = np.random.randint(0, 2)
-            d3 = np.random.randint(0, 2)
-            d4 = np.random.randint(0, 2)
-            d5 = np.random.randint(0, 2)
-            d6 = np.random.randint(0, 2)
-            d7 = np.random.randint(0, 100)
-            d8 = np.random.randint(0, 2)
-            data = [tm, d1, d2, d3, d4, d5, d6, d7, d8]
-            payload = '{{"tm":"{0}","d1":{1},"d2":{2},"d3":{3},"d4":{4},"d5":{5},"d6":{6},"d7":{7},"d8":{8}}}'.format(*data)
-            print 'Digital: {}\n'.format(payload)
-            publish.single(
-                    topic='{}/{}'.format(self.password, self.device_id),
-                    payload=payload,
-                    hostname=self.host,
-                    auth={'username': self.username,
-                        'password': self.password})
+            self._make_data()
+            self._send_data()
             time.sleep(self.global_period)
+
+    def _make_data(self):
+        tm = time.time()
+        d1 = np.random.randint(0, 2)
+        d2 = np.random.randint(0, 2)
+        d3 = np.random.randint(0, 2)
+        d4 = np.random.randint(0, 2)
+        d5 = np.random.randint(0, 2)
+        d6 = np.random.randint(0, 2)
+        d7 = np.random.randint(0, 100)
+        d8 = np.random.randint(0, 2)
+        self.data = [tm, d1, d2, d3, d4, d5, d6, d7, d8]
+
+    def _send_data(self):
+        payload = '{{"tm":"{0}","d1":{1},"d2":{2},"d3":{3},"d4":{4},"d5":{5},"d6":{6},"d7":{7},"d8":{8}}}'.format(*self.data)
+        print 'Digital: {}\n'.format(payload)
+        publish.single(topic='{}/{}'.format(self.password, self.device_id),
+                payload=payload, hostname=self.host,
+                auth={'username': self.username, 'password': self.password})
 
 
 class AnalogSensors(threading.Thread):
@@ -120,33 +126,36 @@ class AnalogSensors(threading.Thread):
         self.host = host
         self.device_id = device_id
         self.global_period = global_period
+        self.t = 0
+        self.dt = 0.1
         self.setDaemon(True)
 
     def run(self):
         time.sleep(5 * np.random.rand())
-        t = 0
-        dt = 0.1
         while True:
-            tm = time.time()
-            a1 = np.random.rand() + 10 + 10*np.sin(2*np.pi*t)
-            a2 = np.random.rand() + 20 + 20*np.sin(2*np.pi*t)
-            a3 = np.random.rand() + 30 + 30*np.sin(2*np.pi*t)
-            a4 = np.random.rand() + 40 + 40*np.sin(2*np.pi*t)
-            a5 = np.random.rand() + 10 + 10*np.cos(2*np.pi*t)
-            a6 = np.random.rand() + 20 + 20*np.cos(2*np.pi*t)
-            a7 = np.random.rand() + 30 + 30*np.cos(2*np.pi*t)
-            a8 = np.random.rand() + 40 + 40*np.cos(2*np.pi*t)
-            data = [tm, a1, a2, a3, a4, a5, a6, a7, a8]
-            payload = '{{"tm":"{0}","a1":{1},"a2":{2},"a3":{3},"a4":{4},"a5":{5},"a6":{6},"a7":{7},"a8":{8}}}'.format(*data)
-            print 'Analog: {}\n'.format(payload)
-            publish.single(
-                    topic='{}/{}'.format(self.password, self.device_id),
-                    payload=payload,
-                    hostname=self.host,
-                    auth={'username': self.username,
-                        'password': self.password})
+            self._make_data()
+            self._send_data()
+            self.t += self.dt
             time.sleep(self.global_period)
-            t += dt
+
+    def _make_data(self):
+        tm = time.time()
+        a1 = np.random.rand() + 10 + 10*np.sin(2*np.pi*self.t)
+        a2 = np.random.rand() + 20 + 20*np.sin(2*np.pi*self.t)
+        a3 = np.random.rand() + 30 + 30*np.sin(2*np.pi*self.t)
+        a4 = np.random.rand() + 40 + 40*np.sin(2*np.pi*self.t)
+        a5 = np.random.rand() + 10 + 10*np.cos(2*np.pi*self.t)
+        a6 = np.random.rand() + 20 + 20*np.cos(2*np.pi*self.t)
+        a7 = np.random.rand() + 30 + 30*np.cos(2*np.pi*self.t)
+        a8 = np.random.rand() + 40 + 40*np.cos(2*np.pi*self.t)
+        self.data = [tm, a1, a2, a3, a4, a5, a6, a7, a8]
+
+    def _send_data(self):
+        payload = '{{"tm":"{0}","a1":{1},"a2":{2},"a3":{3},"a4":{4},"a5":{5},"a6":{6},"a7":{7},"a8":{8}}}'.format(*self.data)
+        print 'Analog: {}\n'.format(payload)
+        publish.single(topic='{}/{}'.format(self.password, self.device_id),
+                payload=payload, hostname=self.host,
+                auth={'username': self.username, 'password': self.password})
 
 
 class Device(object):

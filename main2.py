17c55201bf37e022c80e6615c55b579197671772
Unknown <imototoriwatomodatijanai@gmail.com>
2017-12-15 13:29:57 +0900

    introduce the upper level class for sensors

--- main2.py
+++ main2.py
@@ -15,16 +15,9 @@ import numpy as np
 from params import *
 
 
-class Beacon(threading.Thread):
-    def __init__(self, username, password, host, device_id, period, message):
-        super(Beacon, self).__init__()
-        self.username = username
-        self.password = password
-        self.host = host
-        self.device_id = device_id
-        self.period = period
-        self.message = message
-        self.setDaemon(True)
+class Sensor(object):
+    def __init__(self):
+        self.sensor_name = 'Sensor'
 
     def run(self):
         time.sleep(5 * np.random.rand())
@@ -34,22 +27,41 @@ class Beacon(threading.Thread):
             time.sleep(self.period)
 
     def _make_data(self):
-        data = [time.time(), self.message]
-        self.payload = '{{"tm":"{0}","Beacon":"{1}"}}'.format(*data)
+        pass
 
     def _send_data(self):
         try:
             publish.single(topic='{}/{}'.format(self.password, self.device_id),
                     payload=self.payload, hostname=self.host,
                     auth={'username': self.username, 'password': self.password})
-            print 'Beacon: {}\n'.format(self.payload)
+            print '{}: {}\n'.format(self.sensor_name, self.payload)
         except:
-            print 'Beacon: The payload was not send.'
+            print '{}: The payload was not send.'.format(self.sensor_name)
 
 
-class EnvironmentalInformation(threading.Thread):
+class Beacon(Sensor, threading.Thread):
+    def __init__(self, username, password, host, device_id, period, message):
+        super(Beacon, self).__init__()
+        super(Sensor, self).__init__()
+        self.sensor_name = 'Beacon'
+        self.username = username
+        self.password = password
+        self.host = host
+        self.device_id = device_id
+        self.period = period
+        self.message = message
+        self.setDaemon(True)
+
+    def _make_data(self):
+        data = [time.time(), self.message]
+        self.payload = '{{"tm":"{0}","Beacon":"{1}"}}'.format(*data)
+
+
+class EnvironmentalInformation(Sensor, threading.Thread):
     def __init__(self, username, password, host, device_id, period):
         super(EnvironmentalInformation, self).__init__()
+        super(Sensor, self).__init__()
+        self.sensor_name = 'EnvInfo'
         self.username = username
         self.password = password
         self.host = host
@@ -58,13 +70,6 @@ class EnvironmentalInformation(threading.Thread):
         self.t = 0
         self.dt = 0.1
         self.setDaemon(True)
-
-    def run(self):
-        time.sleep(5 * np.random.rand())
-        while True:
-            self._make_data()
-            self._send_data()
-            time.sleep(self.period)
 
     def _make_data(self):
         eiLPrs = np.random.rand() + 900 + 100*np.sin(2*np.pi*self.t)
@@ -75,19 +80,12 @@ class EnvironmentalInformation(threading.Thread):
         data = [time.time(), eiLPrs, seaPrs, eiTemp, eiHumi]
         self.payload = '{{"tm":"{0}","eiLPrs":{1},"seaPrs":{2},"eiTemp":{3},"eiHumi":{4}}}'.format(*data)
 
-    def _send_data(self):
-        try:
-            publish.single(topic='{}/{}'.format(self.password, self.device_id),
-                    payload=self.payload, hostname=self.host,
-                    auth={'username': self.username, 'password': self.password})
-            print 'EnvInfo: {}\n'.format(self.payload)
-        except:
-            print 'EnvInfo: The payload was not send.'
 
-
-class DigitalSensors(threading.Thread):
+class DigitalSensors(Sensor, threading.Thread):
     def __init__(self, username, password, host, device_id, port_ids, period):
         super(DigitalSensors, self).__init__()
+        super(Sensor, self).__init__()
+        self.sensor_name = 'Digital sensor'
         self.username = username
         self.password = password
         self.host = host
@@ -96,27 +94,11 @@ class DigitalSensors(threading.Thread):
         self.period = period
         self.setDaemon(True)
 
-    def run(self):
-        time.sleep(5 * np.random.rand())
-        while True:
-            self._make_data()
-            self._send_data()
-            time.sleep(self.period)
-
     def _make_data(self):
         data = ['"tm":{0}'.format(time.time())]
         for port_id in self.port_ids:
             data.append('"d{0}":{1}'.format(port_id, np.random.randint(0, 2)))
         self.payload = '{{{0}}}'.format(','.join(data))
-
-    def _send_data(self):
-        try:
-            publish.single(topic='{}/{}'.format(self.password, self.device_id),
-                    payload=self.payload, hostname=self.host,
-                    auth={'username': self.username, 'password': self.password})
-            print 'Digital sensor: {}\n'.format(self.payload)
-        except:
-            print 'Digital sensor: The payload was not send.'
 
 
 class DigitalCounters(object):
@@ -132,9 +114,11 @@ class DigitalCounters(object):
             counter.start()
 
 
-class DigitalCounter(threading.Thread):
+class DigitalCounter(Sensor, threading.Thread):
     def __init__(self, username, password, host, device_id, port_id, period):
         super(DigitalCounter, self).__init__()
+        super(Sensor, self).__init__()
+        self.sensor_name = 'Digital counter'
         self.username = username
         self.password = password
         self.host = host
@@ -143,24 +127,8 @@ class DigitalCounter(threading.Thread):
         self.period = period
         self.setDaemon(True)
 
-    def run(self):
-        time.sleep(5 * np.random.rand())
-        while True:
-            self._make_data()
-            self._send_data()
-            time.sleep(self.period)
-
     def _make_data(self):
         self.payload = '{{"tm":"{0}","d{1}":{2}}}'.format(time.time(), self.port_id, np.random.randint(0, 50))
-
-    def _send_data(self):
-        try:
-            publish.single(topic='{}/{}'.format(self.password, self.device_id),
-                    payload=self.payload, hostname=self.host,
-                    auth={'username': self.username, 'password': self.password})
-            print 'Digital counter: {}\n'.format(self.payload)
-        except:
-            print 'Digital counter: The payload was not send.'
 
 
 class DigitalElements(object):
@@ -176,24 +144,19 @@ class DigitalElements(object):
         self.counters.run()
 
 
-class AnalogSensors(threading.Thread):
-    def __init__(self, username, password, host, device_id, global_period):
+class AnalogSensors(Sensor, threading.Thread):
+    def __init__(self, username, password, host, device_id, period):
         super(AnalogSensors, self).__init__()
+        super(Sensor, self).__init__()
+        self.sensor_name = 'Analog sensor'
         self.username = username
         self.password = password
         self.host = host
         self.device_id = device_id
-        self.global_period = global_period
+        self.period = period
         self.t = 0
         self.dt = 0.1
         self.setDaemon(True)
-
-    def run(self):
-        time.sleep(5 * np.random.rand())
-        while True:
-            self._make_data()
-            self._send_data()
-            time.sleep(self.global_period)
 
     def _make_data(self):
         a1 = np.random.rand() + 10 + 10*np.sin(2*np.pi*self.t)
@@ -207,15 +170,6 @@ class AnalogSensors(threading.Thread):
         self.t += self.dt
         data = [time.time(), a1, a2, a3, a4, a5, a6, a7, a8]
         self.payload = '{{"tm":"{0}","a1":{1},"a2":{2},"a3":{3},"a4":{4},"a5":{5},"a6":{6},"a7":{7},"a8":{8}}}'.format(*data)
-
-    def _send_data(self):
-        try:
-            publish.single(topic='{}/{}'.format(self.password, self.device_id),
-                    payload=self.payload, hostname=self.host,
-                    auth={'username': self.username, 'password': self.password})
-            print 'Analog: {}\n'.format(self.payload)
-        except:
-            print 'Analog: The payload was not send.'
 
 
 class Device(object):

fa16c127ebaaec039e59f2872986d16ee3b609a5
Unknown <imototoriwatomodatijanai@gmail.com>
2017-12-25 12:06:32 +0900

    modify the column names of server data

--- matching.py
+++ matching.py
@@ -23,33 +23,9 @@ tmp_data = tmp_data.loc[begin:end, 'a1':'a8']
 local_data = {sensor_name: tmp_data}
 
 # load the data downloaded from the server as server_data
-headers = ['time'] + ['sd{}'.format(i+1) for i in xrange(8)] + ['sa{}'.format(i+1) for i in xrange(8)] + ['seiTemp', 'seiHumi', 'seiLPrs', 'sseaPrs', 'sbeacon', 'smessage', 'dummy']
+headers = ['time'] + ['d{}_s'.format(i+1) for i in xrange(8)] + ['a{}_s'.format(i+1) for i in xrange(8)] + ['eiTemp_s', 'eiHumi_s', 'eiLPrs_s', 'seaPrs_s', 'beacon_s', 'message_s', 'dummy_s']
 server_data_raw = pd.read_csv('./receivedata.csv', header=0, names=headers).set_index('time')
-# server_data = {sensor_name: server_data_raw.query('index in @local_data[@sensor_name].index').loc[:, 'sa1':'sa8']}
-server_data = {sensor_name: server_data_raw.loc[begin:end, 'sa1':'sa8'].dropna(how='all', axis=0)}
+server_data = {sensor_name: server_data_raw.loc[begin:end, 'a1_s':'a8_s'].dropna(how='all', axis=0)}
 
 df = pd.concat([local_data[sensor_name], server_data[sensor_name]], axis=1)
 print np.logical_and(local_data[sensor_name], server_data[sensor_name])
-
-
-'''
-# Load the .csv file
-time.sleep(5)
-num = 1
-dic = {}
-if num == 1:
-    csv_name = 'receivedata.csv'
-elif num > 1:
-    csv_name = 'receivedata ({:d}).csv'.format(num - 1)
-csv_path = 'C:/Users/admin/Downloads/{}'.format(csv_name)
-
-if os.path.exists(csv_path):
-    with open(csv_path, 'r') as f:
-        reader = csv.reader(f)
-        header = next(reader)
-        dic['Kenshouki{:04d}'.format(num)] = [csv_name, reader]
-else:
-    pass
-
-num += 1
-'''

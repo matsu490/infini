2d4f46df6d5328f3fe597afcf4c0d6b7e0a18fd5
Unknown <imototoriwatomodatijanai@gmail.com>
2017-12-27 11:32:00 +0900

    at -> time_stamp

--- matching.py
+++ matching.py
@@ -11,10 +11,9 @@ import pandas as pd
 
 device_id = 1
 sensor_names = ['Envinfo', 'Analog_sensors', 'Digital_sensors', 'Digital_counters', 'Beacon']
-# sensor_names = ['Digital_counters']
-at = '20171226165147'
-begin = '2017-12-27 09:40:36'
-end = '2017-12-27 09:43:00'
+time_stamp = '20171227102729'
+begin = '2017-12-27 10:28:00'
+end = '2017-12-27 10:47:00'
 
 
 # load the data downloaded from the server as server_data
@@ -23,7 +22,7 @@ server_data_raw = pd.read_csv('./receivedata.csv', header=0, names=headers).set_
 
 for sensor_name in sensor_names:
     # load the data stored by python as local_data
-    file_path = './Logs/IFT_ML1-YONEZAWA{:04d}/{}_{}.csv'.format(device_id, sensor_name, at)
+    file_path = './Logs/IFT_ML1-YONEZAWA{:04d}/{}_{}.csv'.format(device_id, sensor_name, time_stamp)
     tmp_data = pd.read_csv(file_path).set_index('time')
     tmp_data = tmp_data.loc[begin:end, :]
     tmp_data = tmp_data.iloc[:, 0:-1]

7e70563fb23c8c9bfbb676c4c21156fc092fabd2
Unknown <imototoriwatomodatijanai@gmail.com>
2017-12-27 10:23:44 +0900

    simplify making data

--- matching.py
+++ matching.py
@@ -11,11 +11,11 @@ import numpy as np
 import pandas as pd
 
 device_id = 1
-# sensor_names = ['Envinfo', 'Analog_sensors', 'Digital_sensors', 'Digital_counters', 'Beacon']
-sensor_names = ['Envinfo', 'Analog_sensors', 'Beacon']
-at = '20171225130606'
-begin = '2017-12-25 13:06:00'
-end = '2017-12-25 13:07:00'
+sensor_names = ['Envinfo', 'Analog_sensors', 'Digital_sensors', 'Digital_counters', 'Beacon']
+# sensor_names = ['Digital_counters']
+at = '20171226165147'
+begin = '2017-12-27 09:40:36'
+end = '2017-12-27 09:43:00'
 
 
 # load the data downloaded from the server as server_data
@@ -26,31 +26,12 @@ for sensor_name in sensor_names:
     # load the data stored by python as local_data
     file_path = './Logs/IFT_ML1-YONEZAWA{:04d}/{}_{}.csv'.format(device_id, sensor_name, at)
     tmp_data = pd.read_csv(file_path).set_index('time')
-    if sensor_name == 'Envinfo':
-        tmp_data = tmp_data.loc[begin:end, 'eiTemp':'seaPrs']
-    elif sensor_name == 'Analog_sensors':
-        tmp_data = tmp_data.loc[begin:end, 'a1':'a8']
-    elif sensor_name == 'Digital_sensors':
-        tmp_data = tmp_data.loc[begin:end, :]
-        tmp_data = tmp_data.iloc[:, 0:-1]
-    elif sensor_name == 'Digital_counters':
-        tmp_data = tmp_data.loc[begin:end, :]
-        tmp_data = tmp_data.iloc[:, 0:-1]
-    elif sensor_name == 'Beacon':
-        tmp_data = tmp_data.loc[begin:end, 'beacon']
+    tmp_data = tmp_data.loc[begin:end, :]
+    tmp_data = tmp_data.iloc[:, 0:-1]
     local_data = {sensor_name: tmp_data}
 
     # extract data of the sensor from the server_data
-    if sensor_name == 'Envinfo':
-        server_data = {sensor_name: server_data_raw.loc[begin:end, 'eiTemp':'seaPrs'].dropna(how='all', axis=0)}
-    elif sensor_name == 'Analog_sensors':
-        server_data = {sensor_name: server_data_raw.loc[begin:end, 'a1':'a8'].dropna(how='all', axis=0)}
-    elif sensor_name == 'Digital_sensors':
-        server_data = {sensor_name: server_data_raw.loc[begin:end, tmp_data.columns].dropna(how='all', axis=0)}
-    elif sensor_name == 'Digital_counters':
-        server_data = {sensor_name: server_data_raw.loc[begin:end, tmp_data.columns].dropna(how='all', axis=0)}
-    elif sensor_name == 'Beacon':
-        server_data = {sensor_name: server_data_raw.loc[begin:end, 'beacon'].dropna(how='all', axis=0)}
+    server_data = {sensor_name: server_data_raw.loc[begin:end, tmp_data.columns].dropna(how='all', axis=0)}
 
     df = pd.concat([local_data[sensor_name], server_data[sensor_name]], axis=1)
     judge_table = local_data[sensor_name] == server_data[sensor_name]
